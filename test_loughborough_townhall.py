5852d5916d9407487614a3fcb060fe486156f2f7
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-06-12 20:44:22 -0700

    Update test_loughborough_townhall.py

--- test_loughborough_townhall.py
+++ test_loughborough_townhall.py
@@ -5,17 +5,17 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
-from scrapers.elgiva.run_extractor import ElgivaExtractor  # noqa: E402
+from scrapers.Loughborough_townhall.run_extractor import LoughboroughtownhallExtractor  # noqa: E402
 from utils.logger import setup_logger  # noqa: E402
 
-logger = setup_logger("test_elgiva", log_to_file=False)
+logger = setup_logger("test_loughborough_townhall", log_to_file=False)
 
 
-def test_elgiva_pipeline():
-    logger.info("Starting Elgiva Pipeline Test")
-    extractor = ElgivaExtractor(
+def test_loughborough_townhal_pipeline():
+    logger.info("Starting loughborough_townhall Pipeline Test")
+    extractor = LoughboroughtownhallExtractor(
         local_test=False,
-        show_count=None,
+        show_count=2,
         save_csv_locally=True,
         csv_incremental_mode=False,
     )
@@ -24,4 +24,4 @@ def test_elgiva_pipeline():
 
 
 if __name__ == "__main__":
-    test_elgiva_pipeline()
+    test_loughborough_townhal_pipeline()
